81de9df02bea748073a2f40f3fccdedce3457163
tolaseadegbite <tolasekelvin@gmail.com>
2025-11-05 22:50:02 +0100

    Add default_url_options to fix host_url issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,4 +82,5 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  Rails.application.routes.default_url_options = { host: "localhost", port: 3000 }
 end
