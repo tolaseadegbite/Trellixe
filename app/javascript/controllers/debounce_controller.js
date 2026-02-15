ac0980d668883df1c4eda382726d5e89892a138a
Tolase Kelvin Adegbite <tolasekelvin@gmail.com>
2026-02-15 11:50:59 +0100

    Add debounce to search forms

---
+++ app/javascript/controllers/debounce_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static values = { delay: { type: Number, default: 300 } }
+
+  submit() {
+    clearTimeout(this.timeout)
+    
+    this.timeout = setTimeout(() => {
+      this.element.requestSubmit()
+    }, this.delayValue)
+  }
+}
